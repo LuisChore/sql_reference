48ff0fea802a64cc1cc8b2290870ead927c038c0
luischore <luis.vazquez.ch@hotmail.com>
2022-08-01 20:29:08 -0500

    SQL Create

--- DDL/03_create_table.sql
+++ DDL/03_create_table.sql
@@ -1,10 +1,11 @@
 
+
+#### CREATE TABLE ####
 /*
-CREATE TABLE table_name({columns});
+CREATE TABLE table_name({column names});
 
 Requirements:
 - Add at least one column
-
 
 CREATE TABLE [IF NOT EXISTS] table_name(
 	column_1 data_type constraints,
@@ -15,21 +16,40 @@ CREATE TABLE [IF NOT EXISTS] table_name(
 
 */
 
+
+use sales_db;
+CREATE TABLE IF NOT EXISTS sales(
+	purchase_number INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    date_of_purchase DATE NOT NULL,
+    customer_id INT,
+    item_code VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS customers(
+	customer_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email_address VARCHAR(255),
+    number_of_complaints INT DEFAULT 0
+);
+#### DROP TABLE #### 
 /*
 	DROP TABLE table_name;
 */
 
-/* CONSTRAINT 
+DROP TABLE sales;
+DROP TABLE customers;
 
-AUTO_INCREMENT
-	Frees  you  from having  to  insert  all primary key numbers manually through the INSERT
+#### AUTO_INCREMENT ####
+/* 
+	Constraint that frees  you  from having  to  insert  all primary key numbers manually through the INSERT
     command at a larger stage. Assigns 1 to the first record of the table and autommatically
     increments by 1 for every subsequent row 
 
 */
 
-
-/* SQL Objects
+#### Using SQL Objects ####
+/* 
 Whenever you would like to refer to an SQL object in your queries, you must specify the 
 database to which it is applied
 
@@ -44,29 +64,7 @@ SQL objects:
 	USE database_name;
     
     
-2) Call a table from a certain database
-	database_name . sql_object
+2) Call a table from a certain database (dot operator)
+	database_name.sql_object
 */
 
-USE sales_db;
-
-CREATE TABLE IF NOT EXISTS sales(
-	purchase_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	date_of_purchase DATE NOT NULL,
-    customer_id INT,
-    item_code VARCHAR(10) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS customers(
-	customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email_adress VARCHAR(255),
-    number_of_compaints INT DEFAULT 0
-);
-
-ALTER TABLE customers
-RENAME COLUMN email_adress TO email_address;
-
-ALTER TABLE customers
-RENAME COLUMN number_of_compaints TO number_of_complaints;
