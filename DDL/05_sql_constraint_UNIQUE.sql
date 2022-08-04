9f94203d3b9ae85284f8c59b7c7d200a69291db6
luischore <luis.vazquez.ch@hotmail.com>
2022-08-03 20:40:27 -0500

    SQL Unique Key

--- DDL/05_sql_constraint_UNIQUE.sql
+++ DDL/05_sql_constraint_UNIQUE.sql
@@ -1,20 +1,16 @@
-# SQL Constraints 
-/*Specific rules or limits that we define in our tables.*/
-
 ##### UNIQUE #####
 
 /*
-Use whenever you would like to specify that you don't want to see duplicate data in a given field 
-
-It ensures that all valus in a column (or a set of columns) are different 
-
-Unique Keys are implemented in SQL through a constraint (UNIQUE constraint) 
+	Use whenever you would like to specify that you don't want to see duplicate data in a given field 
+	It ensures that all valus in a column (or a set of columns) are different 
+	Unique Keys are implemented in SQL through a constraint (UNIQUE constraint) 
 */
 
 use sales_db;
+DROP TABLE sales;
 DROP TABLE customers;
 
-CREATE TABLE IF NOT EXISTS customers(
+CREATE TABLE customers(
 	customer_id INT,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
@@ -26,7 +22,6 @@ UNIQUE KEY(email_address)
 
 
 # add it after the table creation with ALTER TABLE 
-
 
 DROP TABLE customers;
 CREATE TABLE IF NOT EXISTS customers(
@@ -45,7 +40,7 @@ ADD UNIQUE KEY(email_address);
 
 
 
-### INDEXES ### 
+##### INDEXES ##### 
 
 /*
 UNIQUE keys in MySQL have the same role as indexes, BUT NOT THE REVERSE
@@ -63,13 +58,12 @@ It takes more time to update a table because indexes must be updated too.
 */
 
 
-### REMOVE AN INDEX ###
+##### REMOVE AN INDEX #####
 
 /*
 ALTER TABLE table_name
 DROP INDEX unique_key_field 
 */
-
 ALTER TABLE customers
 DROP INDEX email_address;
 
@@ -81,18 +75,18 @@ DROP INDEX email_address;
 
 DROP TABLE customers;
 
-CREATE TABLE IF NOT EXISTS customers(
-	customer_id INT AUTO_INCREMENT,
-    email_address VARCHAR(255),
+CREATE TABLE customers(
+	customer_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    number_of_complaints INT,
-    PRIMARY KEY(customer_id) 
+    email_address VARCHAR(255),
+    number_of_complaints INT DEFAULT 0,
+    UNIQUE KEY(email_address),
+    PRIMARY KEY(customer_id)
 );
-
 
 ALTER TABLE customers
 ADD COLUMN gender ENUM('M','F') AFTER last_name;
 
-INSERT INTO customers(first_name,last_name,gender,email_address, number_of_complaints) 
-VALUES('John','Macky','M','john.mckinley@365careers.com',0);
+INSERT INTO customers(first_name,last_name,gender,email_address) 
+VALUES('John','Macky','M','john.mckinley@365careers.com');
