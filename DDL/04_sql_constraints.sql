a7e5c27b256641bf5395f55b302770f5a9db5d1c
luischore <luis.vazquez.ch@hotmail.com>
2022-08-03 20:40:11 -0500

    SQL Constraints

--- DDL/04_sql_constraints.sql
+++ DDL/04_sql_constraints.sql
@@ -1,4 +1,4 @@
-# SQL Constraints 
+/**SQL Constraints **/
 /*Specific rules or limits that we define in our tables.*/
 
 
@@ -9,44 +9,40 @@ Use of AUTO_INCREMENT is only used for
 -UNIQUE KEY
 -INDEX
 
-To include more than one columns in the primary key
+To include one or more columns in the primary key
 	PRIMARY KEY(column1,column2,...) 
 */
 
 USE sales_db;
 
-DROP TABLE sales;
 CREATE TABLE IF NOT EXISTS sales(
-	purchase_number INT AUTO_INCREMENT,
-    date_of_purchase DATE,
+	purchase_number INT NOT NULL AUTO_INCREMENT,
+    date_of_purchase DATE NOT NULL,
     customer_id INT,
     item_code VARCHAR(10),
     PRIMARY KEY(purchase_number)
 );
 
-DROP TABLE customers;
 CREATE TABLE IF NOT EXISTS customers(
-	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+	customer_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     email_address VARCHAR(255),
-	number_of_complaints INT DEFAULT 0
+    number_of_complaints INT DEFAULT 0,
+    PRIMARY KEY(customer_id) 
 );
 
-DROP TABLE items;
 CREATE TABLE IF NOT EXISTS items(
-	item_code VARCHAR(255) NOT NULL,
+	item_code VARCHAR(255) NOT NULL PRIMARY KEY,
     item VARCHAR(255) NOT NULL,
     unit_price NUMERIC(10,2) NOT NULL,
-    company_id VARCHAR(255),
-    PRIMARY KEY(item_code)
+    company_id VARCHAR(255)
 );
 
-DROP TABLE companies;
 CREATE TABLE IF NOT EXISTS companies(
 	company_id VARCHAR(255) NOT NULL,
     company_name VARCHAR(255) NOT NULL,
-    headquarters_phone_number INT,
+    headquarters_phone_number INT(12),
     PRIMARY KEY(company_id)
 );
 
@@ -54,62 +50,34 @@ CREATE TABLE IF NOT EXISTS companies(
 ##### FOREIGN KEY #####
 /*
 It points to a column for another table and, thus, links the two tables
-The  foreing key mantains the referentian integrity within the database
+The  foreing key constraint mantains the referential integrity within the database
 
 PARENT TABLE - REFERENCED TABLE
-- A set of columns represent its PRIMARY KEY
+	A set of columns that represent its PRIMARY KEY
 
 CHILD TABLE - REFERENCING TABLE
-- A set of columns represent a FOREING KEY (a relation with the PARENT TABLE) 
+	A set of columns representing a FOREING KEY (a relation with the PARENT TABLE,
+    this set of columns refers to the columns in the PARENT TABLE) 
 
+SINTAX IN CHILD TABLE
+	FOREIGN KEY(column_name,...) REFERENCES parent_table(column_name,...) 
 
 OPTIONS:
 	ON DELETE CASCADE:
 		if a specific value from the parent table's primary key has been deleted
         all the records from the child table referring to this  values  will  be
         removed as well
-
-
 */
 
 /*FIRST OPTION (don't delete the table)*/
-DROP TABLE sales;
-CREATE TABLE IF NOT EXISTS sales(
-	purchase_number INT AUTO_INCREMENT,
-    date_of_purchase DATE,
-    customer_id INT,
-    item_code VARCHAR(10),
-    PRIMARY KEY(purchase_number)
-);
-
-DROP TABLE customers;
-CREATE TABLE IF NOT EXISTS customers(
-	customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email_address VARCHAR(255),
-	number_of_complaints INT DEFAULT 0
-);
 
 ALTER TABLE sales
 ADD FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 ON DELETE CASCADE;
 
 
-
-
-
 /*SECOND OPTION (Delete the table)*/
 DROP TABLE sales;
-
-DROP TABLE customers;
-CREATE TABLE IF NOT EXISTS customers(
-	customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email_address VARCHAR(255),
-	number_of_complaints INT DEFAULT 0
-);
 
 CREATE TABLE IF NOT EXISTS sales(
 	purchase_number INT AUTO_INCREMENT,
@@ -123,7 +91,7 @@ CREATE TABLE IF NOT EXISTS sales(
 
 
 
-/** DROP A FOREING KEY**/
+##### DROP A FOREING KEY ##### 
 # a name of the foreing key is created automatically
 
 ALTER TABLE sales
